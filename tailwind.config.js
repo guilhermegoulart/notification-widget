d6b5e794533e6d30d9029688349c6c65fd682aea
Guilherme Goulart <guilhermegoulartm@outlook.com>
2023-07-12 21:24:10 -0300

    creating the base structure for the widget page, and addin a Icon librarie.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        xxs: '0.625rem'
+      }
+      ,
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
