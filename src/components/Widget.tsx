a64aa8596421d134950be68f261a2336643e5567
Guilherme Goulart <guilhermegoulartm@outlook.com>
2023-08-17 08:30:49 -0300

    edditing src/app/layout.tsx and src/components/Widget.tsx

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -85,6 +85,14 @@ export function Widget() {
               <span>Há 3 minutos</span>
             </div>
           </div>
+          <div className="flex gap-2 self-center">
+              <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-400 dark:bg-zinc-800 hover:bg-zinc-500 dark:hover:bg-zinc-700">
+                <X className="w-3 h-3 text-zinc-50 "/>
+              </button>
+              <button className="w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-violet-600">
+                <Check className="w-3 h-3 text-zinc-50"/>
+              </button>
+            </div>
         </div>
       </div>
     </div>
